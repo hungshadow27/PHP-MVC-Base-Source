34c08ffcaccc57822730744131537dbf4e430a81
hungshadow27 <you@example.com>
2024-01-17 14:12:49 +0700

    change logo

--- src/Views/Home.view.php
+++ src/Views/Home.view.php
@@ -1,6 +1,6 @@
 <?php require_once "./src/Views/layouts/header.php"; ?>
 <div style="width: 100%; text-align: center; margin-top: 40px;">
-    <img id="logo" src="Public/images/hungshadow.jpg" alt="">
+    <img id="logo" src="Public/images/hungshadow.png" alt="">
     <h1>HELLO WORLD FROM HOME</h1>
 </div>
 
